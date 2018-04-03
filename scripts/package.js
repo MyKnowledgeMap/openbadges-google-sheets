f1406c107416f90381fda26a9eca273b9426342e
Harry Mitchenson <harrymitchinson@icloud.com>
2018-04-03 13:58:09 +0100

    Update manifest and version handling

--- scripts/package.js
+++ scripts/package.js
@@ -1,18 +1,16 @@
 const fs = require("fs");
 const archiver = require("archiver");
-const name = "sheets";
+const name = "forms";
 
 // Fetch the manifest file.
 const manifestFile = `./chrome/manifest.json`;
 const manifest = JSON.parse(fs.readFileSync(manifestFile));
-// Split the current version string "x.x.x"
-const versions = manifest.version.split(".");
-// Remove the latest version of the patch value.
-const latestVersion = new Number(versions.splice(2, 1));
-// Add the new patch value.
-versions.push(process.env.CIRCLE_BUILD_NUM || latestVersion + 1);
+// Get the latest version as a number.
+const latestVersion = new Number(manifest.version);
+//Get the new version numberr.
+const newVersion = process.env.CIRCLE_BUILD_NUM || latestVersion + 1;
 // Build the version string.
-manifest.version = versions.join(".");
+manifest.version = newVersion.toString();
 // Write the updated manifest back to the file.
 fs.writeFileSync(manifestFile, JSON.stringify(manifest, null, 2));
 console.log(`Version updated to ${manifest.version}`);
