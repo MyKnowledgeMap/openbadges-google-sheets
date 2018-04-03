1f7feb5f9d7bdcc1af8383dc28f7c50e589836ee
Harry <harrymitchinson@icloud.com>
2018-04-03 12:13:33 +0100

    Move forms into separate repo (#54)
    Removed all forms add-on stuff from this repo

--- scripts/package.js
+++ scripts/package.js
@@ -1,34 +1,41 @@
 const fs = require("fs");
 const archiver = require("archiver");
-
 const name = "sheets";
+
+// Fetch the manifest file.
+const manifestFile = `./chrome/manifest.json`;
+const manifest = JSON.parse(fs.readFileSync(manifestFile));
+// Split the current version string "x.x.x"
+const versions = manifest.version.split(".");
+// Remove the latest version of the patch value.
+const latestVersion = new Number(versions.splice(2, 1));
+// Add the new patch value.
+versions.push(process.env.CIRCLE_BUILD_NUM || latestVersion + 1);
+// Build the version string.
+manifest.version = versions.join(".");
+// Write the updated manifest back to the file.
+fs.writeFileSync(manifestFile, JSON.stringify(manifest, null, 2));
+console.log(`Version updated to ${manifest.version}`);
 
 // The output package.
 const package = `./dist/${name.toLowerCase()}.zip`;
-
 // Create the output file.
 const output = fs.createWriteStream(package);
 const archive = archiver("zip", {
   zlib: { level: 9 } // Sets the compression level.
 });
-
 // Throw any errors, need to stop processing.
 archive.on("error", function(err) {
   throw err;
 });
-
 // Pipe the archive to the output file.
 archive.pipe(output);
-
 // Add the entry file to the archive.
 archive.append(fs.createReadStream(`./dist/${name}.js`), {
   name: "Code.gs"
 });
-
 // Add the chrome folder contents.
-archive.directory(`./chrome/${name.toLowerCase()}/`, false);
-
+archive.directory(`./chrome/`, false);
 // Finalize the archive.
 archive.finalize();
-
 console.log("Packaged successfully.");
--- src/interfaces/google-helpers.ts
+++ src/interfaces/google-helpers.ts
@@ -1,28 +1,7 @@
 /**
- * Interface for FormSubmitEvent since it doesn't appear to be included in @types/google-app-script.
- * https://developers.google.com/apps-script/guides/triggers/events
- * @interface FormSubmitEvent
- */
-interface IFormSubmitEvent {
-  source: GoogleAppsScript.Forms.Form;
-  response: GoogleAppsScript.Forms.FormResponse;
-  authMode: GoogleAppsScript.Script.AuthMode;
-  triggerUid: number;
-}
-
-/**
  * Simple helper interface which saves typing Array<Array<number | boolean | string | Date>> for range.GetValues().
  * @interface IGetValuesResult
  * @extends {(Array<Array<number | boolean | string | Date>>)}
  */
 interface IGetValuesResult
   extends Array<Array<number | boolean | string | Date>> {}
-
-/**
- * Simple form item response model.
- * @interface ISimpleItemResponse
- */
-interface ISimpleItemResponse {
-  title: string;
-  response: any; // string | string[] | string[][]
-}
