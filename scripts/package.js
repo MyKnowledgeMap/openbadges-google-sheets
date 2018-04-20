af7dc9563897aac62cbdbd93f4e3c97379294934
Harry <harrymitchinson@icloud.com>
2018-04-20 13:27:05 +0100

    Revert "Rework package step (#62)"
    This reverts commit 860abb5d132e85f14121b41dc723a1334e0d0bdc.

--- scripts/package.js
+++ scripts/package.js
@@ -1,40 +1,39 @@
-const fs = require("fs-extra");
-const archive = require("archiver")("zip", { zlib: { level: 9 } });
-const template = require("../package.json").manifest;
+const fs = require("fs");
+const archiver = require("archiver");
+const name = "sheets";
+
+// Fetch the manifest file.
+const manifestFile = `./chrome/manifest.json`;
+const manifest = JSON.parse(fs.readFileSync(manifestFile));
+// Get the latest version as a number.
+const latestVersion = new Number(manifest.version);
+//Get the new version numberr.
+const newVersion = process.env.CIRCLE_BUILD_NUM || latestVersion + 1;
+// Build the version string.
+manifest.version = newVersion.toString();
+// Write the updated manifest back to the file.
+fs.writeFileSync(manifestFile, JSON.stringify(manifest, null, 2));
+console.log(`Version updated to ${manifest.version}`);
 
 // The output package.
-const package = `./dist/sheets.zip`;
-
-// Throw any errors.
-archive.on("error", (err) => {
+const package = `./dist/${name.toLowerCase()}.zip`;
+// Create the output file.
+const output = fs.createWriteStream(package);
+const archive = archiver("zip", {
+  zlib: { level: 9 } // Sets the compression level.
+});
+// Throw any errors, need to stop processing.
+archive.on("error", function(err) {
   throw err;
 });
-
 // Pipe the archive to the output file.
-archive.pipe(fs.createWriteStream(package));
-
-// Generate the updated manifest file.
-const manifest = (() => {
-  let version;
-  if (process.env.CIRCLE_BUILD_NUM) {
-    version = process.env.CIRCLE_BUILD_NUM;
-  } else {
-    const current = Number(template.version);
-    version = `${current + 1}`;
-  }
-  return JSON.stringify({ ...template, version }, null, 2);
-})();
-archive.append(manifest, { name: "manifest.json" });
-
+archive.pipe(output);
 // Add the entry file to the archive.
-const code = fs.createReadStream(`./dist/sheets.js`);
-archive.append(code, { name: "Code.gs" });
-// Add empty background.js file.
-archive.append("", { name: "background.js" });
-// Add the assets folder contents.
-archive.directory(`./assets/`, false);
-
+archive.append(fs.createReadStream(`./dist/${name}.js`), {
+  name: "Code.gs"
+});
+// Add the chrome folder contents.
+archive.directory(`./chrome/`, false);
 // Finalize the archive.
 archive.finalize();
-fs.removeSync(`./dist/sheets.js`);
 console.log("Packaged successfully.");
