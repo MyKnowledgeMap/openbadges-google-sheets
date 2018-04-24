84cadddab11528322a4e8bc7ef64b7bfb4a7ecd0
Harry <harrymitchinson@icloud.com>
2018-04-24 16:15:25 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,8 @@
+process.on("unhandledRejection", (err) => {
+  console.error(err);
+  throw err;
+});
+
 require("dotenv-safe").config();
 const fs = require("fs");
 const webStore = require("chrome-webstore-upload")({
@@ -12,27 +17,21 @@ const name = "sheets";
 const package = `./dist/${name}.zip`;
 
 // Fetch a token for uploading and publishing.
-webStore
-  .fetchToken()
-  .then(async (token) => {
-    // Get the package.
-    const archive = fs.createReadStream(package);
+webStore.fetchToken().then(async (token) => {
+  // Get the package.
+  const archive = fs.createReadStream(package);
 
-    // Upload the package.
-    const uploadResult = await webStore.uploadExisting(archive, token);
-    if (uploadResult.uploadState !== "SUCCESS") {
-      throw uploadResult;
-    }
-    console.log(`Uploaded {${uploadResult.id}} to Chrome Webstore.`);
+  // Upload the package.
+  const uploadResult = await webStore.uploadExisting(archive, token);
+  if (uploadResult.uploadState !== "SUCCESS") {
+    throw uploadResult;
+  }
+  console.log(`Uploaded {${uploadResult.id}} to Chrome Webstore.`);
 
-    // Publish the updated add-on.
-    const publishResult = await webStore.publish("default", token);
-    if (publishResult.status[0] !== "OK") {
-      throw publishResult;
-    }
-    console.log(`Published {${publishResult.item_id}} on Chrome Webstore.`);
-  })
-  .catch((err) => {
-    console.error(err);
-    throw err;
-  });
+  // Publish the updated add-on.
+  const publishResult = await webStore.publish("default", token);
+  if (publishResult.status[0] !== "OK") {
+    throw publishResult;
+  }
+  console.log(`Published {${publishResult.item_id}} on Chrome Webstore.`);
+});
